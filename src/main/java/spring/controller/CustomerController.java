2e33763f2d72177b7fddd1ab6b95b49623a44e5f
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-04-13 13:41:28 +0100

    Completed Udemy Spring CRUD application

---
+++ src/main/java/spring/controller/CustomerController.java
@@ -0,0 +1,60 @@
+package spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring.entity.Customer;
+import spring.service.CustomerService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/list")
+    public String listCustomers(Model theModel){
+
+        List<Customer> theCustomers = customerService.getCustomers();
+        theModel.addAttribute("customers", theCustomers);
+
+        return "list-customers";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        Customer theCustomer = new Customer();
+        theModel.addAttribute("customer", theCustomer);
+
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+        System.out.println("We are in the Post Mapping!");
+
+        customerService.saveCustomer(theCustomer);
+        System.out.println("Method performed!");
+
+        return "redirect:/customer/list";
+    }
+
+    @GetMapping("showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel){
+        Customer theCustomer = customerService.getCustomer(theId);
+        theModel.addAttribute("customer", theCustomer);
+        return "customer-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCustomer(@RequestParam("customerId") int theId) {
+        customerService.deleteCustomer(theId);
+        return "redirect:/customer/list";
+    }
+
+}
---
+++ src/main/java/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.DAO.CustomerDAO;
+import spring.entity.Customer;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Transactional
+    public List<Customer> getCustomers() {
+
+        return customerDAO.getCustomers();
+    }
+
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Transactional
+    public Customer getCustomer(int theId) {
+        return customerDAO.getCustomer(theId);
+    }
+
+    @Transactional
+    public void deleteCustomer(int theId) {
+        customerDAO.deleteCustomer(theId);
+    }
+}
